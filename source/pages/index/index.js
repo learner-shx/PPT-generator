34b19bb70291d564bec1eebce9fb465c3d985d51
luzhixing12345 <luzhixing12345@163.com>
2022-05-30 18:13:47 +0800

    [feat] : finish index page

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,48 +1,48 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/index/index.js
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      arr: [],
+      movies: [{
+          url: '../../images/Rotating/3.jpg'
+        },
+        {
+          url: '../../images/Rotating/news.png'
+        },
+        {
+          url: '../../images/Rotating/1.jpg'
+        },
+        {
+          url: '../../images/Rotating/2.jpg'
+        }
+      ],
+    },
+  
+    async onLoad() {
+      var that = this;
+      const db = wx.cloud.database({ // 链接数据表
+        env: "cloud1-9gv9ynmtc4528521"
+      });
+      db.collection('announcement').where({}).get({
+        success: function (res) {
+          let all = []
+          // res.data 包含该记录的数据
+          for (let i = 0; i < 3; i++) {
+            all.push(res.data[i])
+          }
+          that.setData({
+            arr: all,
+          })
+        }
+      })
+    },
+  
+    navigator() {
+      wx.reLaunch({
+        url: "../hall/hall"
       })
     }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+  })
