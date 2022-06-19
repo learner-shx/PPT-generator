9380de2ca79d866d478f3d01ed660e7029a2774e
luzhixing12345 <luzhixing12345@163.com>
2022-06-19 17:53:12 +0800

    [feat] : add user_type

--- source/pages/user/user.js
+++ source/pages/user/user.js
@@ -36,7 +36,7 @@ Page({
             that.setData({
               userInfo : app.globalData.userInfo
             })
-            wx.setStorageSync('userInfo', app.globalData.userInfo);
+            
           },
           fail: function(res) {
             console.log(res)
@@ -49,9 +49,9 @@ Page({
           _openid : app.globalData.userInfo._openid
         }).get({
           success(res) {
-            console.log(res)
             if(res.data.length==0) {
               // 没有搜索到则新建用户
+              app.globalData.userInfo.user_type = false;
               wx.cloud.database().collection('user').add({
                 // data 字段表示需新增的 JSON 数据
       
@@ -59,13 +59,16 @@ Page({
                   userName: app.globalData.userInfo.nickName,
                   avatarUrl: app.globalData.userInfo.avatarUrl,
                   email: "未填写",
-                  call: "未填写"
+                  call: "未填写",
+                  user_type : false
                 },
               })
+            } else {
+              app.globalData.userInfo = res.data[0]
             }
+            wx.setStorageSync('userInfo', app.globalData.userInfo);
           }
         })
-
         
       }
     })
@@ -97,7 +100,8 @@ Page({
             avatarUrl: res.data[0].avatarUrl,
             userName: res.data[0].userName,
             email: res.data[0].email,
-            call: res.data[0].call
+            call: res.data[0].call,
+            user_type: res.data[0].user_type
           })
         }
       })
