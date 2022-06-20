55fc1b37f22c7053c4e0302abaa778fec81f3bda
realxuzijun <1548059731@qq.com>
2022-06-20 20:57:38 +0800

    information

--- source/pages/user/user.js
+++ source/pages/user/user.js
@@ -12,6 +12,15 @@ Page({
     userInfo: {},
     about: "none",
     imageBase64:[],
+    information_info: {}
+  },
+  onShow()
+  {
+    this.setData({
+      information_info : wx.getStorageSync('information_info')
+    })
+    console.log(this.data.information_info)
+    console.log(app.globalData.information_info)
   },
 
   getUserProfile(e) {
@@ -37,7 +46,8 @@ Page({
             // 拿到用户的OpenId
             app.globalData.userInfo._openid = res.result.userInfo.openId
             that.setData({
-              userInfo : app.globalData.userInfo
+              userInfo : app.globalData.userInfo,
+
             })
             
           },
