a2cc345a697fb51821fac68865753e13c6366d0d
luzhixing12345 <luzhixing12345@163.com>
2022-06-22 19:17:31 +0800

    [feat] : pages style for generator

--- source/pages/generator/generator.js
+++ source/pages/generator/generator.js
@@ -1,82 +1,35 @@
 // pages/generator/generator.js
-// const PptxGenJS = require('pptxgenjs')
 Page({
 
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        PPT_style: ['自然', '艺术', '学术', '中国风'],
+        PPT_style_index: 0
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
 
+    ppttest() {
+
+        wx.cloud.callFunction({
+            name: 'pptxGenJsWriteFile',
+            data: {
+                // ppt : pres, // 这个 CloudID 值到云函数端会被替换
+                filepath: 'ppttest.pptx',
+            },
+            success(res) {
+                console.log(res)
+            }
+        })
+    },
+    selctPPTstyle() {
+        var that = this
+        wx.showActionSheet({
+            itemList: that.data.PPT_style,
+            success: function (res) {
+                that.setData({
+                    PPT_style_index: res.tapIndex
+                })
+            }
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    },
-    ppttest:function(e)
-    {
-
-      wx.cloud.callFunction({
-        name: 'pptxGenJsWriteFile',
-        data: {
-          // ppt : pres, // 这个 CloudID 值到云函数端会被替换
-          filepath : 'ppttest.pptx',
-        },
-        success(res)
-        {
-          console.log(res)
-        }
-      })
-    }
 })
