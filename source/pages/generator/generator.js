c5b503561fcfc17584f05e9a251567a601631338
luzhixing12345 <luzhixing12345@163.com>
2022-06-28 07:45:35 +0800

    [feat] : finish PPT style and user input page

--- source/pages/generator/generator.js
+++ source/pages/generator/generator.js
@@ -1,23 +1,60 @@
-// pages/generator/generator.js
+
 Page({
 
     data: {
-        PPT_style: ['serif', 'simple', 'sky', 'solarized','简约ppt'],
-        PPT_style_index: 4,
-        description: "",
-        describe: "",
+        PPT_title: "",
         downloadUrl: "",
         filePath: "",
-        time : 0
+        time : 0,
+        subPPT_pages : [],
+        PPT_style_config : {
+            PPT_style: ['简约ppt', 'serif', 'simple', 'sky', 'solarized',],
+            PPT_style_index: 0,
+            subPPTpage_number: ['1', '2', '3', '4'],
+            subPPTpage_number_index: 1,
+            intelligentPictureMap: true
+        },
     },
 
 
     handleInput(e) {
         clearTimeout(this.data.time)
         var that = this;
+        var id = e.target.id;
         this.data.time = setTimeout(() => {
-            that.getDescription(e.detail.value)
+            if (id == 'PPT_title') {
+                that.setPPTtitle(e.detail.value)
+            } else {
+                if (id[6] == 'c') {
+                    // subPPTcontent_0
+                    var index = id.substring(14);
+                    console.log(index)
+                    that.setSubPPTcontent(index,e.detail.value)
+                } else {
+                    var index = id.substring(12);
+                    console.log(index)
+                    that.setSubPPTtitle(index,e.detail.value)
+                }
+            }
         }, 300)
+    },
+
+    setSubPPTcontent(index,value) {
+        var subPPT_pages = this.data.subPPT_pages;
+        subPPT_pages[index].content = value;
+        
+        this.setData({
+            subPPT_pages : subPPT_pages
+        })
+    },
+
+    setSubPPTtitle(index,value) {
+        var subPPT_pages = this.data.subPPT_pages;
+        subPPT_pages[index].title = value;
+        
+        this.setData({
+            subPPT_pages : subPPT_pages
+        })
     },
 
     onLoad() {
@@ -29,30 +66,70 @@ Page({
     },
     onShow() {
         this.setData({
-            PPT_style_index: 0,
             downloadUrl: "",
-
         })
     },
-    getDescription(value) {
+    setPPTtitle(value) {
         this.setData({
-            description: value,
+            PPT_title: value,
         })
     },
 
+    deleteItem(e) {
+        var subPPT_pages = this.data.subPPT_pages;
+        var index = e.currentTarget.dataset.index;
+        console.log(index)
+        subPPT_pages.splice(index, 1);
+        this.setData({
+            subPPT_pages : subPPT_pages
+        })
+    },
+
+    moreOptions() {
+        var that = this;
+        wx.navigateTo({
+          url: './styleSelect/styleSelect?id1=' + that.data.PPT_style_config.PPT_style_index + "&id2=" + that.data.PPT_style_config.subPPTpage_number_index + "&id3=" + that.data.PPT_style_config.intelligentPictureMap,
+          events : {
+            backFromTargetPage : function (backData) {
+                console.log(backData.data)
+                that.setData({
+                    PPT_style_config : {
+                        PPT_style_index : backData.data.PPT_style_index,
+                        subPPTpage_number_index : backData.data.subPPTpage_number_index,
+                        intelligentPictureMap : backData.data.intelligentPictureMap
+                    }
+                })
+                console.log('return')
+            }
+          }
+        })
+    },
+
+
+    addItem() {
+        var subPPT_pages = this.data.subPPT_pages;
+        var subPPT_page = {
+            title : "",
+            content : ""
+        }
+        subPPT_pages.push(subPPT_page);
+        this.setData({
+            subPPT_pages : subPPT_pages
+        })
+    },
+ 
     oneTouchGenerator() {
 
-        if (this.data.description == ''){
+        if (this.data.PPT_title == ''){
             wx.showToast({
-              title: '文案不能为空',
+              title: '标题不能为空',
               icon : 'none',
               duration : 500
             })
             return;
         }
         var that = this;
-        // console.log(that.data.PPT_style[that.data.PPT_style_index]);
-        var suffix;
+
         if (that.data.PPT_style[that.data.PPT_style_index] == '简约ppt') {
             suffix = '.pptx';
         }
@@ -77,36 +154,6 @@ Page({
         that.setData({
           downloadUrl: 'res.fileList[0].tempFileURL',
         });
-
-        // // var that = this;
-        // var fileList;
-        // console.log('cloud://kamilu-3g69c1hh0c963d36.6b61-kamilu-3g69c1hh0c963d36-1312241224/' + that.data.filePath + suffix);
-        // var url;
-        // wx.cloud.getTempFileURL({
-        //     fileList: [{
-        //         fileID: 'cloud://kamilu-3g69c1hh0c963d36.6b61-kamilu-3g69c1hh0c963d36-1312241224/' + that.data.filePath + suffix,
-        //         maxAge: 60 * 60, // one hour
-        //     }]
-        // }).then(res => {
-        //     // get temp file URL
-        //     // res.fileList[0]);
-        //     console.log(res);
-        //     that.setData({
-        //         downloadUrl: 'res.fileList[0].tempFileURL',
-                
-        //     });
-        //     if (res.fileList[0].tempFileURL != ''){
-        //         wx.showToast({
-        //           title: 'ppt生成成功',
-        //           icon : 'none',
-        //           duration : 500
-        //         })
-        //         return;
-        //     }
-        // }).catch(error => {
-        //     console.log(fileList[0]);
-        // });
-        // console.log(112);
     },
 
 
@@ -144,17 +191,6 @@ Page({
         });
 
     },
-    selectPPTstyle() {
-        var that = this
-        wx.showActionSheet({
-            itemList: that.data.PPT_style,
-            success: function (res) {
-                console.log(res)
-                that.setData({
-                    PPT_style_index: res.tapIndex,
-                })
-            }
-        })
-    },
+
 
 })
---
+++ source/pages/generator/styleSelect/styleSelect.js
@@ -0,0 +1,71 @@
+
+const app = getApp();
+
+Page({
+
+    data: {
+        PPT_style: ['简约ppt', 'serif', 'simple', 'sky', 'solarized',],
+        // PPT_style_index: 0,
+        subPPTpage_number: ['1', '2', '3', '4'],
+        // subPPTpage_number_index: 1,
+        intelligentPictureMap: true
+    },
+
+    switchChange() {
+        this.setData({
+            intelligentPictureMap: !this.data.intelligentPictureMap
+        })
+    },
+
+    onLoad(option) {
+        console.log(option)
+        this.setData({
+            PPT_style_index: option.id1,
+            subPPTpage_number_index: option.id2,
+            intelligentPictureMap: option.id3 == "false" ? false : true
+        })
+        console.log("here")
+    },
+
+    selectPPTstyle() {
+        var that = this
+        wx.showActionSheet({
+            itemList: that.data.PPT_style,
+            success: function (res) {
+                console.log(res)
+                that.setData({
+                    PPT_style_index: res.tapIndex,
+                })
+            }
+        })
+    },
+
+    selectPPTextendPageNumber() {
+        var that = this
+        wx.showActionSheet({
+            itemList: that.data.subPPTpage_number,
+            success: function (res) {
+                console.log(res)
+                that.setData({
+                    subPPTpage_number_index: res.tapIndex,
+                })
+            }
+        })
+    },
+
+
+    onUnload() {
+        app.globalData.targetPageEventChannel = this.getOpenerEventChannel();
+        var that = this;
+        setTimeout(function () {
+            var t = getApp().globalData.targetPageEventChannel
+            t ? t.emit('backFromTargetPage', {
+                data: {
+                    PPT_style_index: that.data.PPT_style_index,
+                    subPPTpage_number_index: that.data.subPPTpage_number_index,
+                    intelligentPictureMap: that.data.intelligentPictureMap
+                }
+            }) : console.log('not targetPage navigateBack, canceled')
+        }, 400)
+    }
+})
